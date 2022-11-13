d317fa1539909ad8d2425afbdd69821c8275fccc
root <root@DESKTOP-KJ4QI5E.localdomain>
2022-11-13 15:28:45 +0000

    just another one

---
+++ test.py
@@ -0,0 +1,24 @@
+
+import sys, random, os
+
+def test(start, end):
+    lst = list(range(start, end+1))
+
+    random.shuffle(lst)
+
+    arg=""
+    for i in lst:
+        arg += f"{i} "
+    os.system(f"./push_swap {arg} | ./checker_linux {arg}")
+    os.system(f"./push_swap {arg} | wc -l")
+
+def main():
+
+    if (len(sys.argv) != 3):
+        return (-42)
+    start = int(sys.argv[1])
+    end = int(sys.argv[2])
+
+    for i in range(0, 100):
+        test(start, end)
+
